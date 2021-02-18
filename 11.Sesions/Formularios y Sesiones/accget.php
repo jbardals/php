9f9d753784cd9c275b4f47068cb099750bf3e5a9
bardals <bardals@bitbucket.org>
2021-02-18 13:09:26 +0100

    form y sesions

---
+++ 11.Sesions/Formularios y Sesiones/accget.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+/* Como hemos utilizado el método GET en el formulario, recogemos los
+ * valores en un array. Cada elemento tiene el mismo nombre que la 
+ * variable del formulario.
+ * 
+ * Y los pasamos al elemento de $_SESSION que se crea en ese instante.
+ * 
+ * */
+ 
+ // Iniciamos la sesión
+ session_start();
+ 
+ // Pasamos los valores del formulario a la sesión
+ if (!empty($_GET['nombre'])){
+	 $_SESSION['nombre'] = $_GET['nombre'];
+ }
+ else {
+	 $_SESSION['nombre'] = "Usuario anónimo";
+ }
+ 
+ // Usamos el operador ternario para hacer lo mismo con el correo pero 
+ // de otro modo.
+ $_SESSION['correo'] = (!empty($_GET['correo'])) ? $_GET['correo'] : 
+ "No tiene";
+ 
+ // El elemento radio siempre devolverá un valor, así que lo asignamos 
+ // directamente.
+ $_SESSION['gustos'] = $_GET['gustos'];
+  
+ // Con esos datos damos la bienvenida al visitante
+ echo "Hola <b>".$_SESSION['nombre']."</b>. le damos la Bienvenida.<br />"; 
+ echo "Sabemos que prefiere <b>".$_SESSION['gustos']."</b>.<br />";
+ echo "Y que podemos contactar en la siguiente dirección <b>".
+ $_SESSION['correo']."</b>.<br /><br />"; 
+ echo "El identificador de sesión es: <b>".session_id()."</b>.";
+?>
+
---
+++ 11.Sesions/Formularios y Sesiones/readme.md
@@ -0,0 +1,7 @@
+﻿
+
+
+> Written with [StackEdit](https://stackedit.io/).
+Crea el siguiente formulario [Form.jpg](Form.jpg)
+a) El [formulario](formpost.php) usará el método POST para enviar los datos a un fichero [accpost.php,](accpost.php) que iniciará una sesión y procesará los datos recogidos del formulario a través de variables de sesión para mostralos por pantalla. Mostrar también el identificador de sesión (aunque esto no se debe hacer por seguridad).
+b)  Repetir el aptdo a) con un formulario que use el método GET para enviar los datos a un fichero [accget.php](accget.php), Qué diferencias observas en la url de envío entre enviar por GET y enviar por POST?
